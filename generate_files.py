1bda2c374e6899d0dc400b8c10e5cb4a0c864652
saarahy <you@example.com>
2017-04-03 11:31:17 -0700

    modifying files - adding option to benchmarks problems

--- generate_files.py
+++ generate_files.py
@@ -5,9 +5,14 @@ config        = yaml.load(open("conf/conf.yaml"))
 def random_floats(low, high, size):
     return [np.random.uniform(low, high) for _ in xrange(size)]
 
-low=0
+low=-1
 high=1
 size=20
+
+low_test=-1
+high_test=1
+size_test=20
+
 problem=config["problem"]
 num_v=config["num_var"]
 num_p=config["n_problem"]
@@ -21,5 +26,5 @@ for n_corr in range(1,num_corr):
     np.savetxt('./data_corridas/%s/train_%d_%d.txt'%(problem,num_p, n_corr),samples,delimiter=',')
 
 for n_corr in range(1, num_corr):
-    samples = random_floats(low, high, size)
+    samples = random_floats(low_test, high_test, size_test)
     np.savetxt('./data_corridas/%s/test_%d_%d.txt' % (problem, num_p, n_corr), samples, delimiter=',')
